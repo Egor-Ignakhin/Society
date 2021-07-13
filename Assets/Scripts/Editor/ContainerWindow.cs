7ce8d2925bfe774a69a04c7bfadc56506bc3772e
”Mrak701” <egor.ignakhin@gmail.com>
2021-07-13 16:19:13 +0300

    work on chest GUI

--- Assets/Scripts/Editor/ContainerWindow.cs
+++ Assets/Scripts/Editor/ContainerWindow.cs
@@ -1,45 +1,126 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
+using Inventory;
+using System.Collections.Generic;
 
 public class ContainerWindow : EditorWindow
 {
-    private Vector2 scrollPos = new Vector2(10, 10);
-
+#if UNITY_EDITOR
+    private Vector2 verticalScrollPosition;
     [MenuItem("Window/GUI Containers Data")]
     public static void ShowWindow()
     {
         GetWindow(typeof(ContainerWindow));
     }
+
     private void OnGUI()
     {
+        autoRepaintOnSceneChange = true;
         GUILayout.Label("Containers Data", EditorStyles.boldLabel);
-        Object selectedObject = Selection.activeObject;
-        if (!(selectedObject as GameObject).TryGetComponent<Inventory.ItemsContainer>(out var container))
+        UnityEngine.Object selectedObject = Selection.activeObject;
+        if (!selectedObject)
+            return;
+        var obj = selectedObject as GameObject;
+
+        if (!obj)
             return;
 
-        EditorGUILayout.LabelField(Selection.activeObject.name);        
-        EditorGUILayout.BeginScrollView(scrollPos, GUILayout.Height(250));
-        //      GUILayout.BeginHorizontal();
-        var data = container.GetStartedData();
-        for (int i = 0; i < data.items.Count; i++)
+        if (!obj.TryGetComponent<ItemsContainer>(out var container))
+            return;
+
+        EditorGUILayout.LabelField($"Container     {Selection.activeObject.name}");
+        (List<int> items, List<int> count) = container.GetStartedData();
+        DrawFreeItems(items.Count, container);
+
+        GUILayout.Space(25);
+
+        DrawContentCount(items.Count);
+
+        DrawCurrentContent(items, count, container);
+    }
+
+    private void DrawFreeItems(int currentCount, ItemsContainer container)
+    {
+        EditorGUILayout.LabelField($"All items");
+        EditorGUILayout.BeginHorizontal();
+        for (int i = 1; i < Enum.GetNames(typeof(ItemStates.ItemsID)).Length - 1; i++)
+        {
+            var iT = Resources.Load<Sprite>($"InventoryItems\\{(ItemStates.ItemsID)i}").texture;
+            if (GUILayout.Button(iT, GUILayout.MaxHeight(position.height / 12.5f), GUILayout.MaxWidth(position.width / 5)) &&
+                (currentCount < ItemsContainer.maxCells))
+            {
+                container.AddStartedItem(i);
+            }
+            if (i % 5 == 0)
+            {
+                GUILayout.EndHorizontal();
+                GUILayout.BeginHorizontal();
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+    private void DrawContentCount(int count)
+    {
+        if (count < ItemsContainer.maxCells)
+        {
+            GUI.contentColor = Color.white;
+        }
+        else
+        {
+            GUI.contentColor = Color.red;
+        }
+        EditorGUILayout.LabelField($"Content used [{count} / {ItemsContainer.maxCells}]");
+        GUI.contentColor = Color.white;
+    }
+    private void DrawCurrentContent(List<int> items, List<int> count, ItemsContainer container)
+    {
+        verticalScrollPosition = EditorGUILayout.BeginScrollView(verticalScrollPosition, GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
+        EditorGUILayout.BeginHorizontal();
+        for (int i = 0; i < items.Count; i++)
         {
             try
             {
+                EditorGUILayout.BeginVertical();
                 Texture2D itemTexture = null;
-                if (((Inventory.ItemStates.ItemsID)data.items[i]) == Inventory.ItemStates.ItemsID.Default)
+                if (((ItemStates.ItemsID)items[i]) == ItemStates.ItemsID.Default)
                 {
-                    itemTexture = new Texture2D(2, 2);
+                    itemTexture = new Texture2D(256, 256);
                 }
                 else
                 {
-                    itemTexture = Resources.Load<Sprite>($"InventoryItems\\{(Inventory.ItemStates.ItemsID)data.items[i]}").texture;
+                    itemTexture = Resources.Load<Sprite>($"InventoryItems\\{(ItemStates.ItemsID)items[i]}").texture;
                 }
-                GUILayout.Label(itemTexture);
+                var height = GUILayout.MaxHeight(position.height / 12.5f);
+                var width = GUILayout.MaxWidth(position.width / 5);
+                if (GUILayout.Button(itemTexture, height, width))
+                {
+                    container.RemoveStartedItem(i);
+                    i--;
+                    continue;
+                }
+
+                string cCount = count[i].ToString();
+                string outCount = GUILayout.TextField(cCount.ToString(), width);
+                int newCount = 1;
+                if (!string.IsNullOrEmpty(outCount))
+                    newCount = Convert.ToInt32(outCount);
+                newCount = Mathf.Clamp(newCount, 1, ItemStates.GetMaxCount(items[i]));
+                container.SetStartedCount(i, newCount);
+                EditorGUILayout.EndVertical();
+                if ((i + 1) % 5 == 0)
+                {
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                }
             }
-            catch
+            catch (IndexOutOfRangeException)
             {
-                Debug.Log($"InventoryItems\\{(Inventory.ItemStates.ItemsID)data.items[i]}");
+                Debug.Log($"Null references on InventoryItems\\{(ItemStates.ItemsID)items[i]}");
             }
-        }        
+        }
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.EndScrollView();
     }
+#endif
 }
--- Assets/Scripts/Inventory/Other/ItemsContainer.cs
+++ Assets/Scripts/Inventory/Other/ItemsContainer.cs
@@ -1,4 +1,4 @@
-﻿using PlayerClasses;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,18 +10,18 @@ namespace Inventory
     public sealed class ItemsContainer : InteractiveObject
     {
         public const int maxCells = 40;
-        [SerializeField] private Types startedType;
-        [Range(0, maxCells)] [SerializeField] private int cellsCount;
+        [HideInInspector] [SerializeField] private Types startedType;
+        [HideInInspector] [Range(0, maxCells)] [SerializeField] private int cellsCount;
         private List<(int id, int count, SMGInventoryCellGun gun)> container = new List<(int id, int count, SMGInventoryCellGun gun)>();
         public List<(int id, int count, SMGInventoryCellGun gun)> GetData() => container;
         private bool isOpened;
         private InventoryEventReceiver inventoryEventReceiver;
-        [SerializeField] private List<int> startedItems = new List<int>();
-        [SerializeField] private List<int> startedCount = new List<int>();
+        [HideInInspector] [SerializeField] private List<int> startedItems = new List<int>();
+        [HideInInspector] [SerializeField] private List<int> startedCount = new List<int>();
         public (List<int> items, List<int> count) GetStartedData() => (startedItems, startedCount);
         private void Start()
-        {
-            inventoryEventReceiver = FindObjectOfType<InventoryContainer>().EventReceiver;
+        {            
+               inventoryEventReceiver = FindObjectOfType<InventoryContainer>().EventReceiver;
             for (int i = 0; i < startedItems.Count; i++)
             {
                 var possibleGun = new SMGInventoryCellGun();
@@ -46,6 +46,7 @@ namespace Inventory
             container = lst;
             isOpened = false;
         }
+#if UNITY_EDITOR
         private void OnValidate()
         {
             startedType = Types.Container_1;
@@ -73,10 +74,37 @@ namespace Inventory
                     (startedItems[i] == 0))
                     startedCount[i] = 0;
             }
+            //фикс когда количество обычных предметов = 0
+            for (int i = 0; i < startedItems.Count; i++)
+            {
+                if ((startedItems[i] > 0) &&
+                    (startedCount[i] == 0))
+                    startedCount[i] = 1;
+            }
         }
+
+        public void RemoveStartedItem(int index)
+        {
+            startedItems.RemoveAt(index);
+            startedCount.RemoveAt(index);
+            cellsCount--;
+        }
+
         private void Reset()
         {
             startedType = Types.Container_1;
         }
+        public void AddStartedItem(int index)
+        {
+            startedItems.Add(index);
+            startedCount.Add(1);
+            cellsCount++;
+        }
+
+        public void SetStartedCount(int index, int newCount)
+        {
+            startedCount[index] = newCount;
+        }
+#endif
     }
 }
