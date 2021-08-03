6be871c49880f0752b8f4cc90031d1a9176a51ab
Yeryomin1 <48363433+Yeryomin1@users.noreply.github.com>
2021-08-04 01:43:57 +0300

    Визуальная часть взрыва бочки

---
+++ Assets/Scripts/Shoot/ExplosiveBarrel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveBarrel : MonoBehaviour, IBulletReceiver
+{
+ 
+    [SerializeField] private GameObject explosion;
+    [SerializeField] private GameObject barrel;
+
+
+
+    public void OnBulletEnter()
+    {
+        explosion.SetActive(true);
+        barrel.SetActive(false);
+    }
+ 
+    void Start()
+    {
+        explosion.SetActive(false);
+    }
+
+    void Update()
+    {
+
+
+
+    }
+}
