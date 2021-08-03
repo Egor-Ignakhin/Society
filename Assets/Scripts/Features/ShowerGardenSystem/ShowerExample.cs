078c4bf0e1671bcfeaf87674591138da902233ee
”Mrak701” <egor.ignakhin@gmail.com>
2021-08-03 19:53:41 +0300

    1 part of development Shower

---
+++ Assets/Scripts/Features/ShowerGardenSystem/ShowerExample.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+namespace Features
+{
+    sealed class ShowerExample : InteractiveObject
+    {
+        [SerializeField] private ShowerGarden showerGarden;
+        [SerializeField] private WaterContent waterContent = new WaterContent();
+        [SerializeField] private ParticleSystem waterEffect;
+        private bool waterIsEnable;
+        public bool WaterIsEnable
+        {
+            get => waterIsEnable; set
+            {
+                var emission = waterEffect.emission;
+                emission.enabled = value;
+                waterIsEnable = value;
+            }
+        }
+        internal bool ChangeEnableWater()
+        {
+            WaterIsEnable = !WaterIsEnable;
+            return WaterIsEnable;
+        }
+
+        private void Start()
+        {
+            WaterIsEnable = false;
+            SetType(nameof(ShowerExample));
+            UpdateWaterDesc();
+        }
+        public override void Interact()
+        {
+            showerGarden.OnInteract(this);
+        }
+        private void Update()
+        {
+            if (WaterIsEnable)
+            {
+                if (waterContent.WaterWeight >= 0)
+                    PourOutWater();
+                else WaterIsEnable = false;
+            }
+        }
+        private void PourOutWater()
+        {
+            waterContent.WaterWeight -= Time.deltaTime;
+            
+            UpdateWaterDesc();
+        }
+        private void UpdateWaterDesc() =>
+            additionalDescription = $" ({Math.Round(waterContent.WaterWeight, 1)}/{waterContent.MaxWaterWeight})";
+        [System.Serializable]
+        public sealed class WaterContent
+        {
+            public float WaterWeight;
+            public float MaxWaterWeight = 20;
+            public WaterContent()
+            {
+                WaterWeight = MaxWaterWeight;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Features/ShowerGardenSystem/ShowerLever.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Features
+{
+    /// <summary>
+    /// рычаг подачи воды для душа
+    /// </summary>
+    sealed class ShowerLever : InteractiveObject
+    {
+        [SerializeField] private Transform pivot;
+        [SerializeField] private ShowerExample showerExp;
+        [SerializeField] private float lowerY, higherY;
+        private void Start()
+        {
+            SetType("ClosedShowerLever");
+        }
+        public override void Interact()
+        {
+            bool nextState = !showerExp.ChangeEnableWater();
+
+            //StopAllCoroutines();
+            // StartCoroutine(LeverAnimator(nextState));
+            Vector3 nextRotate = new Vector3(nextState ? higherY : lowerY, pivot.localEulerAngles.y, pivot.localEulerAngles.z);
+            pivot.localEulerAngles = nextRotate;
+            SetType(nextState ? "ClosedShowerLever" : "OpenedShowerLever");
+        }
+
+        private IEnumerator LeverAnimator(bool nextState)
+        {
+            SetType("None");
+            Quaternion nextRotate = new Quaternion(Quaternion.Euler((nextState ? higherY : lowerY), 0, 0).x, 0, 0, 0);
+            while (true)
+            {
+                pivot.localRotation = Quaternion.RotateTowards(pivot.localRotation, nextRotate, 1);
+                if (pivot.localRotation == nextRotate)
+                {
+                    SetType(nextState ? "ClosedShowerLever" : "OpenedShowerLever");
+                    break;
+                }
+                print(1);
+                yield return null;
+            }
+        }
+    }
+}
