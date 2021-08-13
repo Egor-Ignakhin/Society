513bb654ba2080a6097ab5fb134345d3102371e9
”Mrak701” <egor.ignakhin@gmail.com>
2021-08-13 16:56:33 +0300

    feat: now you can change thr version of application(Settings)

---
+++ Assets/Scripts/ApplicationTools/ApplicationVersion.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+namespace Tools
+{
+    [CreateAssetMenu(fileName = "ApplicationVersion", menuName = "Tools/ApplicationVersion", order = 1)]
+    sealed class ApplicationVersion : ScriptableObject
+    {
+        public int MajorVersion;
+        public int MinorVersion;
+        public int BuildVersion;
+        public int RevisionVersion;
+
+        public override string ToString()
+        {
+            return $"{MajorVersion}.{MinorVersion}.{BuildVersion}.{RevisionVersion}";
+        }
+    }
+}
